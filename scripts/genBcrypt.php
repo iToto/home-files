8e05aaae7cdd8f0181ac69ea6e220986ca801a3f
Salvatore D'Agostino <toto19@gmail.com>
2018-07-25 23:16:52 -0400

    initial import of iTerm settings

--- scripts/genBcrypt.php
+++ scripts/genBcrypt.php
@@ -11,5 +11,7 @@ $inputString = bin2hex(getRandBytes(16));
 $options = ['cost' => 11, 'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)];
 $bcryptedString = password_hash($inputString, PASSWORD_BCRYPT, $options);
 
+
+
 echo "16 byte bcrypted string: $bcryptedString\n";
 echo "16 byte string: $inputString";
